cdc46c66eccef8f085f056532919891dd76eb848
Great Ochuko <greatochuko@gmail.com>
2023-10-25 23:41:02 +0100

    debug

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,6 @@ import bcrypt from "bcryptjs";
 
 export async function login(req, res) {
   const { email, username, password } = req.body;
-  console.log(req.body);
   const user =
     (await User.findOne({ email })) || (await User.findOne({ username }));
   if (user) {
