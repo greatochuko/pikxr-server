2149a83bd49d556b1345fe0054bd5f474374f071
Great Ochuko <greatochuko@gmail.com>
2023-10-25 16:46:35 +0100

    changed the default cover photo url to placeholder-cover-photo.png

--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ const UserSchema = new mongoose.Schema(
       minlength: [6, "Password must be greater than 6 characters"],
     },
     imageUrl: { type: String, default: "placeholderProfileImage.png" },
-    coverPhotoUrl: { type: String, default: "placeholderProfileImage.png" },
+    coverPhotoUrl: { type: String, default: "placeholder-cover-photo.png" },
     followers: {
       type: [mongoose.SchemaTypes.ObjectId],
       default: [],
