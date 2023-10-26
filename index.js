42347dadf8046a6bce64700a6c2f4798cafa9ea4
Great Ochuko <greatochuko@gmail.com>
2023-10-26 17:34:17 +0100

    added vercel.json file

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 const MONGODB_URI = "mongodb://127.0.0.1:27017/pikxrDB";
 
-var whitelist = ["http://127.0.0.1:5173", "https://pikxr.onrender.com"];
+var whitelist = ["http://localhost:5173", "https://pikxr.onrender.com"];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.includes(origin) || origin === undefined) {
