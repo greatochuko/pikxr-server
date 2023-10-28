1abbec4bbd0619c819a50c2c1ce0b7e97420869f
Great Ochuko <greatochuko@gmail.com>
2023-10-28 16:01:58 +0100

    added try-catch block in upload photo

--- utils/uploadPhoto.js
+++ utils/uploadPhoto.js
@@ -1,14 +1,18 @@
 export async function uploadPhoto(file) {
-  const response = await fetch(
-    "https://www.filestackapi.com/api/store/S3?key=ATmxns9QQ2OhhpR6rmotZz",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "image/png",
-      },
-      body: file.data,
-    }
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      "https://www.filestackapi.com/api/store/S3?key=ATmxns9QQ2OhhpR6rmotZz",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "image/png",
+        },
+        body: file.data,
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return { error: err.message };
+  }
 }
